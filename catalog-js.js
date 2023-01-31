1bd79264e3dcd85b912a0ff18586d4b8b369540d
woodwardtw <twwoodward@me.com>
2023-01-31 09:27:31 -0500

    modal switch

--- catalog-js.js
+++ catalog-js.js
@@ -1,9 +1,22 @@
 if(document.querySelector('#mailing')){
-	const mailButton = document.querySelector('#mailing');
-	mailButton.addEventListener("click", showForm);
+const mailButton = document.querySelector('#mailing');
+mailButton.addEventListener("click", showForm);
+  const modalClose = document.querySelector('#closeModal');
+  modalClose.addEventListener("click", hideModal);
+   var formDiv = document.querySelector('#myModal');
 }
 
 function showForm(){
-	const formDiv = document.querySelector('#m-form');
-	formDiv.classList.toggle('hide');
+  formDiv.style.display = "block";
+  formDiv.focus();
+}
+
+function hideModal(){
+  formDiv.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == formDiv) {
+    formDiv.style.display = "none";
+  }
 }
